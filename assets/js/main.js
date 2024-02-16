b147fb5694dbba52d677932dc686a03fcf5c6079
antden99 <antoniodenigris@icloud.com>
2024-02-16 15:05:52 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+/*
+Esercizio 1
+Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+*/
+
+const number = 100; //number
+console.log(number);
+
+/*
+Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+Esercizio 2/B
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+*/
+
+const PI = 3.14; //number
+console.log(PI);
+
+// 2/B non posso assegnare un valore ad una variabile const, dovrei utilizzare la variabile let
+
+/*
+Esercizio 2/C
+Dichiara una variabile chiamata radius e assegnale il valore 8.
+Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+Stampa il valore della variabile circle in console.
+*/
+
+const radius = 8; //number
+const circle = ((radius * 2) * PI); //number
+console.log(circle);
+
+/*
+Esercizio 3
+Dichiara una variabile chiamata name e
+assegnale il valore "Carlo".
+Successivamente, modifica il valore della variabile in "Marco".
+Stampa il nuovo valore in console.
+*/
+
+let name = "Carlo"; //string
+name = "Marco"; //string
+console.log(name);
+
+
+
